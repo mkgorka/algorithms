a16464363a445cf46412ca8b6ca36001df311483
mkgorka <mkgorka93@gmail.com>
2021-07-11 19:56:35 +0200

    added description

--- HTML-dynamic-color-string-generator/main.py
+++ HTML-dynamic-color-string-generator/main.py
@@ -3,13 +3,12 @@ import string
 
 
 def generate_color_rgb():
+    """ Generates a valid randomly generated hex color string"""
     digits = string.digits
     letters = string.ascii_letters
     hash_symbol = "#"
-    random_color_rgb = "".join(random.choices(letters+digits, k=6))
+    random_color_rgb = "".join(random.choices(letters + digits, k=6))
 
     hex_color = hash_symbol + random_color_rgb
 
-
-    print(hex_color)
-generate_color_rgb()
+    return hex_color
