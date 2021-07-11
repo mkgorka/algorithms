7627eec39f4bd372ab5f2e324a9cbe3b9dd1d20b
Magdalena Gorka <32820924+mkgorka@users.noreply.github.com>
2021-07-11 18:15:20 +0200

    implemented pig-latin algorithm

---
+++ pig-latin/main.py
@@ -0,0 +1,16 @@
+def pig_it(text):
+    suffix = "ay"
+    text_splt = text.split(" ")
+    result = []
+
+    for word in text_splt:
+        if word.isalpha():
+            first_letter = word[0]
+            temp_lst = list(word)
+            temp_lst[0] = ""
+            n_word = "".join(temp_lst) + first_letter
+            res = n_word+suffix
+            result.append(res)
+        else:
+            result.append(word)
+    return " ".join(result)
