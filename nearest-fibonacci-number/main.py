ba43b8f2756ec33fd1d672329d4317cf49612ff0
mkgorka <mkgorka93@gmail.com>
2021-08-06 00:30:37 +0200

    improved performance

--- nearest-fibonacci-number/main.py
+++ nearest-fibonacci-number/main.py
@@ -1,14 +1,13 @@
-import math
-
-
-def nearest_fibonacci(number):
+def nearest_fibonacci(number: int):
     """Finds the nearest Fibonacci number to a given positive integer(number).
     If there are more than one Fibonacci numbers with equal distance to the given number, return the smallest one."""
     fib_numbers = [0, 1]
     num_1, num_2 = 0, 1
-    min_diff = 0
+    min_diff = number
+    nearest_fib_number: int
+    min_diff_dct = {}
 
-    for num in range(0, number):
+    while num_2 < number:
         next_num = num_1 + num_2
         fib_numbers.append(next_num)
         num_1 = num_2
@@ -17,8 +16,10 @@ def nearest_fibonacci(number):
     if number in fib_numbers:
         nearest_fib_number = number
     else:
-        for idx, numb in range(fib_numbers):
-            num_diff = abs(numb-number)
+        for numb in fib_numbers:
+            num_diff = abs(numb - number)
             if num_diff <= min_diff:
                 min_diff = num_diff
+                min_diff_dct[numb] = min_diff
+        nearest_fib_number = min(min_diff_dct, key=min_diff_dct.get)
     return nearest_fib_number
